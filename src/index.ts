d5a29028679c9150b01e04494bc58dfa626455e8
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2023-08-09 07:42:51 +0600

    generate using string manipulation

--- src/index.ts
+++ src/index.ts
@@ -4,47 +4,35 @@ const DEFAULT_INCREMENTED_VALUE = 1;
 
 let prevTimeStamp = getTimeStamp();
 let incrementedValue = 1;
-const MAX_43_BIT = 0b11111111111111111111111111111111111111111111; // 41 ones in binary
-const MAX_10_BIT = 0b1111111111;
+let maxIncrementedValue = 9;
+
 
 export default function (option?: IOptions) {
   const deviceId = option?.deviceId || getRandomNumber();
-  const timestamp = getTimeStamp();
+  if(deviceId < 10){
+    maxIncrementedValue = 99;
+  }
+  let timestamp = getTimeStamp();
   let incrementedValue = getIncrementedValue(timestamp);
+  while(incrementedValue > maxIncrementedValue){
+    timestamp = getTimeStamp();
+    incrementedValue = getIncrementedValue(timestamp);
+  }
   return generateUniqueNumber(timestamp, deviceId, incrementedValue);
 }
 
+/** @internal */
 export function generateUniqueNumber(
   timestamp: number,
   deviceId: number,
   incrementedValue: number
 ) {
-  const timestamp_43_bit = convertToDesiredBit(timestamp, MAX_43_BIT);
-  const deviceId_10_bit = convertToDesiredBit(deviceId, MAX_10_BIT);
-  const incrementedValue_10_bit = convertToDesiredBit(
-    incrementedValue,
-    MAX_10_BIT
-  );
-
-  return (
-    0 << 63
-    // (timestamp_43_bit << 19) | (deviceId_10_bit << 10) | incrementedValue_10_bit
-  );
-}
-
-/** @internal */
-export function convertToDesiredBit(num: number, MAX_BIT: number) {
-  // convert to 64 bit
-  //
-  const num_big_int = BigInt(num);
-  const num_64 = BigInt.asUintN(64, num_big_int);
-  const MAX_BIT_Big_int = BigInt(MAX_BIT);
-  return num_64 & MAX_BIT_Big_int;
+ return Number(timestamp.toString() + ""+deviceId.toString()+""+incrementedValue.toString());
 }
 
 /** @internal */
 export function getRandomNumber() {
-  return Math.floor(Math.random() * 1024);
+  return Math.floor(Math.random() * 9);
 }
 
 /** @internal */
--- test/unit.test.ts
+++ test/unit.test.ts
@@ -1,12 +1,11 @@
 import generateUniqueNumber, {
-  convertToDesiredBit,
   getIncrementedValue,
   getRandomNumber,
   getTimeStamp,
+  generateUniqueNumber as gen
 } from "../src";
-
-const MAX_43_BIT = 0b011111111111111111111111111111111111111111111; // 41 ones in binary
-const MAX_10_BIT = 0b1111111111;
+import 'node:fs'
+import * as fs from "fs";
 
 describe("Unit test", () => {
   describe("timestamp test", () => {
@@ -20,7 +19,7 @@ describe("Unit test", () => {
     it("should return valid incremental value", () => {
       const timestamp = getTimeStamp();
 
-      for (let i = 1; i < 4096; i++) {
+      for (let i = 1; i < 9; i++) {
         expect(getIncrementedValue(timestamp)).toBe(i);
       }
 
@@ -30,40 +29,40 @@ describe("Unit test", () => {
 
   describe("Random number", () => {
     it("Number should be between 0 and 1023", () => {
-      for (let i = 0; i < 10000; i++) {
+      for (let i = 0; i < 1000; i++) {
         const number = getRandomNumber();
-        expect(number).toBeLessThanOrEqual(1023);
+        expect(number).toBeLessThanOrEqual(9);
         expect(number).toBeGreaterThanOrEqual(0);
       }
     });
   });
 
-  describe("binary conversion test", () => {
-    it("Test timestamp", () => {
-      const timestamp = 1691400287731;
-      const timestamp_43_bit = convertToDesiredBit(timestamp, MAX_43_BIT);
-      console.log({
-        timestamp: decToBin(timestamp << 20),
-        timestamp_43_bit: decToBin(timestamp_43_bit),
-      });
-      console.log(timestamp_43_bit);
-      expect(timestamp_43_bit).toBeDefined();
-    });
-  });
+  describe("Unique id generation test",()=>{
+    it("should match number",()=>{
+      const timestamp = 2540570400000; // July 5, 2050
+      const deviceId = 3;
+      const incrementalValue = 45;
+      const uniqueNumber = gen(timestamp,deviceId,incrementalValue);
+      const uniqueNumber2 = gen(timestamp,incrementalValue,deviceId);
+      expect(uniqueNumber).toBe(2540570400000345);
+      expect(uniqueNumber2).toBe(2540570400000453);
+    })
+  })
 
-  // describe("Unique number", () => {
-  //   it("should return unique number every time", () => {
-  //     const obj: Record<number, boolean> = {};
-  //     for (let i = 0; i < 10000000000; i++) {
-  //       const number = generateUniqueNumber();
-  //       console.log((number >>> 0).toString(2));
-  //       expect(obj[number]).toBeFalsy();
-  //       obj[number] = true;
-  //     }
-  //   });
-  // });
+  describe("Unique number test",()=>{
+
+    it("Should return unique number",()=>{
+        const storage : Record<number, boolean>={};
+        for(let i =0; i< 1000;i++){
+          const num = generateUniqueNumber({deviceId: 0});
+          expect(storage[num]).toBeFalsy();
+          storage[num] = true;
+        }
+
+        fs.writeFileSync(__dirname+"/"+"numlist.log", JSON.stringify(storage))
+
+    })
+  })
+
 });
 
-function decToBin(number) {
-  return (number >>> 0).toString(2);
-}
